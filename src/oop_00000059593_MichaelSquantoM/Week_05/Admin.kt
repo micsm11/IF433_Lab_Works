9a893d5b888cc6b6f26e953a4e8a067cef461ee2
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 20:13:55 +0700

    week05: create Admin class inheriting Pegawai

---
+++ src/oop_00000059593_MichaelSquantoM/Week_05/Admin.kt
@@ -0,0 +1,11 @@
+package oop_00000059593_MichaelSquantoM.Week_05
+
+class Admin(nama: String) : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang duduk di depan komputer melayani administrasi.")
+    }
+
+    fun doAdminWork() {
+        println("[$nama] sedang merekap data absensi mahasiswa.")
+    }
+}
