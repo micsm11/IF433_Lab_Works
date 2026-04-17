14bfbc7be52c335161427e1fcf887c4797669633
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 16:59:42 +0700

    week01: add calculateStatus function

--- src/oop_00000059593_MichaelSquantoM/Week_01/GradingSystem.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_01/GradingSystem.kt
@@ -15,4 +15,6 @@ fun main() {
 
     println("Grade kamu: $grade")
 
+    println("Status: ${calculateStatus(score)}")
 }
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
