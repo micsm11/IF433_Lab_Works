5044e02d80e3e367e369e13ce12540ea50b106b3
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 16:47:49 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000059593_MichaelSquantoM/Week_01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_00000059593_MichaelSquantoM.Week_01
+
+fun main() {
+    var name: String = "Michael Squan"
+    var score: Int = 80
+
+    println("Nama: " + name +_+, "Nilai: " + score)
+
+}
