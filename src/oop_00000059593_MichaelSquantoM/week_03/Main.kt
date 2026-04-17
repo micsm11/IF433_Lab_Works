e039a4687affd144fd5a8dad00d55056700ac04d
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 19:47:06 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_00000059593_MichaelSquantoM/week_03/Main.kt
+++ src/oop_00000059593_MichaelSquantoM/week_03/Main.kt
@@ -11,5 +11,13 @@ fun main() {
 
     println("Pajak yang harus dibayar: ${e.tax}")
 
+    fun main() {
+        val sword = Weapon("Excalibur")
 
+        sword.damage = -50
+
+        sword.damage = 9999
+
+        println("Senjata: ${sword.name}, Damage: ${sword.damage}, Tier: ${sword.tier}")
+    }
 }
