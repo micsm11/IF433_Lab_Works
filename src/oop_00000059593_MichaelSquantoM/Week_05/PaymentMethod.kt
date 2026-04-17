e3442908f41bd92a2c7677bb52d77bb4b09db11d
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 20:19:44 +0700

    week05: (task 2) create EWallet class

--- src/oop_00000059593_MichaelSquantoM/Week_05/PaymentMethod.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_05/PaymentMethod.kt
@@ -3,3 +3,19 @@ package oop_00000059593_MichaelSquantoM.Week_05
 abstract class PaymentMethod(val accountName: String) {
     abstract fun processPayment(amount: Double)
 }
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("[$accountName] Pembayaran E-Wallet sebesar $amount Berhasil.")
+        } else {
+            println("[$accountName] Saldo tidak cukup.")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("[$accountName] Berhasil Top Up: $amount. Saldo baru: $balance")
+    }
+}
