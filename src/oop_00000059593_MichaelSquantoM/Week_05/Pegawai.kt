27318b843814ad436dfc9c0ead01d921bed9a1af
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 20:12:32 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000059593_MichaelSquantoM/Week_05/Pegawai.kt
@@ -0,0 +1,6 @@
+package oop_00000059593_MichaelSquantoM.Week_05
+
+abstract class Pegawai(val nama: String) {
+    // Abstract function tidak memiliki body (tanpa kurung kurawal)
+    abstract fun bekerja()
+}
