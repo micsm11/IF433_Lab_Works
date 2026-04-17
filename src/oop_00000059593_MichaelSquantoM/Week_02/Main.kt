3fabe78e4fd9a0f7c1b22d922d9a5dbcfed04950
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 17:29:13 +0700

    week02: implement selection logic for constructors

--- src/oop_00000059593_MichaelSquantoM/Week_02/Main.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_02/Main.kt
@@ -1,6 +1,5 @@
-package oop_00000059593_MichaelSquantoM.Week_02
-
 import java.util.Scanner
+import javax.xml.transform.Source
 
 fun main() {
     val scanner = Scanner(System.`in`)
@@ -13,19 +12,31 @@ fun main() {
     print("Masukkan NIM (Wajib 5 Karakter): ")
     val nim = scanner.next()
 
-    scanner.nextLine() // Bersihkan buffer newline (penyakit klasik Scanner!)
+    scanner.nextLine()
 
-    // Validasi di sisi pemanggil (Main)
     if (nim.length != 5) {
         println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-        // Program berhenti di sini untuk mahasiswa ini, tidak membuat objek
     } else {
-        print("Masukkan Jurusan: ")
-        val major = scanner.nextLine()
+        print("Pilih Jalur (1. Reguler, 2. Umum): ")
+        val type = scanner.nextInt()
+        scanner.nextLine() // Consume newline
 
-        // Instansiasi Objek karena data sudah aman
-        val s1 = Student(name, nim, major)
+        if (type == 1) {
+            print("Masukkan Jurusan: ")
+            val major = scanner.nextLine()
 
-        println("Status: Pendaftaran selesai.")
+            // Memanggil Primary Constructor
+            val s1 = Student(name, nim, major)
+            println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+            println("Status: Pendaftaran Selesai.")
+
+        } else if (type == 2) {
+            val s2 = Student(name, nim)
+            println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+            println("Status: Pendaftaran Selesai.")
+
+        } else {
+            println("Pilihan ngawur, pendaftaran batal!")
+        }
     }
 }
--- src/oop_00000059593_MichaelSquantoM/Week_02/Student.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_02/Student.kt
@@ -1,23 +1,18 @@
-package oop_00000059593_MichaelSquantoM.Week_02
-
 class Student (
     val name: String,
     val nim: String,
+    var major: String,
     var gpa: Double = 0.0,
-    var major: String
-
 ){
-    constructor(name:String,  nim:String) : this(name, nim, "Non-Matriculated")
-    println("Log: Mmenggunakan constructor jalur semua umum (Tanpa jurusan)")
-
-    class Student (val name: String, val nim: String, var major: String) {
-        init {
-            // Validasi Sederhana: Cek panjang NIM
-            if (nim.length != 5) {
-                println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-                println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-            } else {
-                println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
-            }
+    constructor(name: String, nim: String) : this(name, nim, major = "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
-    }}
+    }
+}
