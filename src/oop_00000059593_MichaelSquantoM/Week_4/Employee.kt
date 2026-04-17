bb916d40edef3f17f3506bc56df209a18fb8884a
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 20:08:20 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_00000059593_MichaelSquantoM/Week_4/Employee.kt
@@ -0,0 +1,11 @@
+package oop_00000059593_MichaelSquantoM.Week_4
+
+open class Employee(val name: String, val baseSalary: Int) {
+    open fun work() {
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int {
+        return (baseSalary * 0.1).toInt()
+    }
+}
