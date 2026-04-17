84405420e751e28436533fa67050d83f526c8ac6
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 22:59:33 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000059593_MichaelSquantoM/Week_06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000059593_MichaelSquantoM.Week_06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
