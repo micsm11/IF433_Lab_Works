30aad34303b21b8b10f9ee86b540b785ceffb69a
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 19:47:54 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_00000059593_MichaelSquantoM/week_03/Player.kt
@@ -0,0 +1,19 @@
+package oop_00000059593_MichaelSquantoM.week_03
+
+class Player(val username: String) {
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount > 0) {
+            val levelSebelum = level
+            xp += amount
+
+            if (level > levelSebelum) {
+                println("Level Up! Selamat $username naik ke level $level")
+            }
+        }
+    }
+}
