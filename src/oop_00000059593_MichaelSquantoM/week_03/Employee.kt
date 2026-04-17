34cebc9ac0a5d331862fa5c3618ccfbd2d55585b
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 17:48:53 +0700

    week03: fix setter using backing field

--- src/oop_00000059593_MichaelSquantoM/week_03/Employee.kt
+++ src/oop_00000059593_MichaelSquantoM/week_03/Employee.kt
@@ -1,11 +1,13 @@
 package oop_00000059593_MichaelSquantoM.week_03
 
-package oop_001_johnthor.week03
-
 class Employee(val name: String) {
     var salary: Int = 0
         set(value) {
-            println("Mencoba set gaji ke: $value")
-            this.salary = value
+            if (value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
+                field = 0
+            } else {
+                field = value
+            }
         }
 }
