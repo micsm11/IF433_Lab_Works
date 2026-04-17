f9962b81536beabdcce9b152d61029d5a7a33b41
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 20:20:52 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000059593_MichaelSquantoM/Week_05/Main.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_05/Main.kt
@@ -28,4 +28,8 @@ fun main() {
     println("Luas Persegi (sisi 5): ${math.hitungLuas(5)}")
     println("Luas Persegi Panjang (5x10): ${math.hitungLuas(5, 10)}")
     println("Luas Lingkaran (r 7.0): ${math.hitungLuas(7.0)}")
+
+    println("\n=== TUGAS 2: PAYMENT SYSTEM ===")
+    val myWallet = EWallet("Dana John", 50000.0)
+    val myCard = CreditCard("Visa John", 100000.0)
 }
