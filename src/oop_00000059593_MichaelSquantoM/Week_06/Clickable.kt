bf620cc4429d992eb8db919d09469b63705bc87a
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 22:52:06 +0700

    week06: create Button class implementing Clickable

--- src/oop_00000059593_MichaelSquantoM/Week_06/Clickable.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_06/Clickable.kt
@@ -3,4 +3,10 @@ package oop_00000059593_MichaelSquantoM.Week_06
 interface Clickable {
     val name: String
     fun click()
+
+    class Button(override val name: String) : Clickable {
+        override fun click() {
+            println("Tombol '$name' berhasil diklik!")
+        }
+    }
 }
