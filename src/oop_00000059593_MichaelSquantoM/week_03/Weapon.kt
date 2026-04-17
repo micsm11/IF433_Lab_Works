ea087ed79b9a123b5633d6b7843d7e95c2f4226c
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 19:46:20 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000059593_MichaelSquantoM/week_03/Weapon.kt
@@ -0,0 +1,25 @@
+package oop_00000059593_MichaelSquantoM.week_03
+
+class Weapon(val name: String) {
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("PERINGATAN: Damage tidak boleh negatif! Nilai tetap: $field")
+                // field tidak berubah (jangan ubah nilai aslinya)
+            } else if (value > 1000) {
+                println("PERINGATAN: Damage terlalu besar (Overpowered)! Dipaksa ke 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() {
+            return when {
+                damage > 800 -> "Legendary"
+                damage > 500 -> "Epic"
+                else -> "Common"
+            }
+        }
+}
