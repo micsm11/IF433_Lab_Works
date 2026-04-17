fd7d8e8b8c17aceee1f2bee3e67edc59a1e378aa
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-17 20:04:28 +0700

    week04: create Car class extending Vehicle

---
+++ src/oop_00000059593_MichaelSquantoM/Week_4/Car.kt
@@ -0,0 +1,7 @@
+package oop_00000059593_MichaelSquantoM.Week_4
+
+open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
+
+    fun openTrunk() {
+        println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+    }
