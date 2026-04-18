c65e832aea82438b3306e85f111f82227d4c82e8
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-18 23:36:39 +0700

    week07: implement data class for structural equality

--- src/oop_00000059593_MichaelSquantoM/Week_07/UserModel.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_07/UserModel.kt
@@ -2,4 +2,4 @@ package oop_00000059593_MichaelSquantoM.Week_07
 
 class RegularUser(val name: String, val age: Int)
 
-data class DataUser(val name: String, val age: In)
+data class DataUser(val name: String, val age: Int)
