ee4e2c4f89304d9784d220765683d1a7e723ca14
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-19 00:32:33 +0700

    week08: (task)create JavaPaymentService for payment intero

---
+++ src/oop_00000059593_MichaelSquantoM/Week_08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000059593_MichaelSquantoM.Week_08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
