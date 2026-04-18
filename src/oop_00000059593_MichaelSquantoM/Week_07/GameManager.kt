a4f56338096054344fe833c2cfcda4db17f25575
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-18 23:45:53 +0700

    week07: (task)create GameManager singleton

---
+++ src/oop_00000059593_MichaelSquantoM/Week_07/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_00000059593_MichaelSquantoM.Week_07
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
