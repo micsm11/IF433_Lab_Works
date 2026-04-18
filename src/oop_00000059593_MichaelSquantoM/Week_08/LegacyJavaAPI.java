92c80713f742f4aa3cf12a67462e557ed6c42535
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-19 00:17:06 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000059593_MichaelSquantoM/Week_08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000059593_MichaelSquantoM.Week_08;
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
