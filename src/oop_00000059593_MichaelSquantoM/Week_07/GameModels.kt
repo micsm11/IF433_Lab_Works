db6104649f99b1c4457381c483c997c7e6a80611
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-18 23:46:53 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000059593_MichaelSquantoM/Week_07/GameModels.kt
@@ -0,0 +1,15 @@
+package oop_00000059593_MichaelSquantoM.Week_07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(30),
+    RARE(10),
+    EPIC(5),
+    LEGENDARY(1)
+}
+
+data class GameItem(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+)
