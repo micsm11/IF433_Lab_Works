2563d061244bfd9f1b7f9f27d3fc878c7472b838
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-18 23:18:56 +0700

    week06: (task) create SmartCCTV with compound interfaces

---
+++ src/oop_00000059593_MichaelSquantoM/Week_06/SmartCCTV.kt
@@ -0,0 +1,21 @@
+package oop_00000059593_MichaelSquantoM.Week_06
+
+class SmartCCTV(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable, Recordable {
+
+    override fun turnOn() {
+        println("CCTV $name dinyalakan.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("CCTV $name dimatikan.")
+        stopRecord()
+    }
+
+    override fun startRecord() {
+        println("CCTV $name mulai merekam.")
+    }
+}
