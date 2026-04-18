734026962e10f6e591361165babe4e80978f10f2
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-19 00:19:37 +0700

    week08: implement safe call with let block

---
+++ src/oop_00000059593_MichaelSquantoM/Week_08/Main.kt
@@ -0,0 +1,24 @@
+package oop_00000059593_MichaelSquantoM.Week_08
+
+fun main() {
+    // --- LATIHAN 2: SAFE CALLS & ELVIS ---
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    // Rantai Safe Calls yang elegan
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota Tidak Diketahui"
+    println("Tujuan pengiriman: $destination")
+    // CHECKPOINT 4: week08: implement chained safe calls and elvis
+
+    // --- LATIHAN 2: SAFE CALL + .LET ---
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+
+    val receipt = validOrder.totalPrice?.let { price ->
+        // Blok ini HANYA jalan jika totalPrice tidak null
+        val tax = price * 0.11
+        "Transaksi Valid. Harga: Rp$price, Pajak: Rp$tax"
+    } ?: "Transaksi Invalid: Harga belum di-set!"
+
+    println(receipt)
+}
---
+++ src/oop_00000059593_MichaelSquantoM/Week_08/UserTest.kt
@@ -0,0 +1,4 @@
+package oop_00000059593_MichaelSquantoM.Week_08
+
+class UserTest {
+}
