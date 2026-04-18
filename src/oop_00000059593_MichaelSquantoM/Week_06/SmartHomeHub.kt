e03017142cb36a2bc353fce7e11df1349eb753cd
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-18 23:21:20 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000059593_MichaelSquantoM/Week_06/SmartHomeHub.kt
@@ -0,0 +1,30 @@
+package oop_00000059593_MichaelSquantoM.Week_06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+    fun activateSecurityMode() {
+        for (device in devices) {
+
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
