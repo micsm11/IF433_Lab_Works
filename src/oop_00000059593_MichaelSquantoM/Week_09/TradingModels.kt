0391e78324f2ea72cf047e9c4c54a86da31b704c
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-24 15:40:53 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000059593_MichaelSquantoM/Week_09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000059593_MichaelSquantoM.Week_09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
