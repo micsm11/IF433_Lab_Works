b590a0638c62e3b79349671fd9a8470703a995c6
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-24 15:27:53 +0700

    week09:implement map for key-value storage

--- src/oop_00000059593_MichaelSquantoM/Week_09/CollectionsBasics.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_09/CollectionsBasics.kt
@@ -18,4 +18,17 @@ fun main () {
     activeUsers.add("UserC")
     activeUsers.add("UserA")
     println("Active Users: $activeUsers")
+
+    println("\n=== TEST MAP ===")
+    val studentGrades = mapOf(
+        "Alice" to "A",
+        "Bob" to "B",
+        "Charlie" to "A" // Value boleh duplikat, Key tidak
+    )
+    println("Nilai Bob: ${studentGrades["Bob"]}")
+
+    val inventory = mutableMapOf("Apples" to 50, "Bananas" to 30)
+    inventory["Oranges"] = 20 // Menambah data baru
+    inventory["Apples"] = 45  // Mengupdate data lama
+    println("Inventory: $inventory")
 }
