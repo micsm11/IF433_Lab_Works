7880a60e42f4980a2be5ca471a2df1a877f1f65f
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-24 15:44:50 +0700

    week09: (task) chain sortedByDescending and map for top performers

--- src/oop_00000059593_MichaelSquantoM/Week_09/TradingDashboard.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_09/TradingDashboard.kt
@@ -21,4 +21,8 @@ fun main() {
 
     val losingTrades = closedTrades.filter { it.roe <= 0 }
 
+    val topPerformersString = winningTrades
+        .sortedByDescending { it.roe }
+        .map { "WIN [${it.pair} - ${it.position}]: +${it.roe}% ROE (Lev: ${it.leverage}x)" }
+
 }
