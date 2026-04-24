ff93565fe1ad337d93e5cda64541a5ac30d8e670
Michael Squanto <MichaelSquanto@gmail.com>
2026-04-24 15:47:10 +0700

    week09: (task) print worst performers and unique pairs

--- src/oop_00000059593_MichaelSquantoM/Week_09/TradingDashboard.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_09/TradingDashboard.kt
@@ -42,4 +42,7 @@ fun main() {
     println("=== CRYPTO TRADING DASHBOARD ===")
     topPerformersString.forEach { println(it) }
 
+    worstPerformersString.forEach { println(it) }
+
+    println("\nUnique Pairs Traded: $uniquePairs")
 }
