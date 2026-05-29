c1785f684027146393baf7d4f799c3e6e14c8aed
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 14:05:07 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000059593_MichaelSquantoM/Week_10/WalletRepository.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_10/WalletRepository.kt
@@ -3,3 +3,17 @@ package oop_00000059593_MichaelSquantoM.Week_10
 interface NamedEntity {
     val name: String
 }
+
+class WalletRepository<T : NamedEntity> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> = items
+
+    fun findByName(name: String): T? {
+        return items.find { it.name.equals(name, ignoreCase = true) }
+    }
+}
