b11c8e94940a9913ed0f49a06d1ede542897f52a
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 14:34:27 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000059593_MichaelSquantoM/Week_11/SmartHomeDashboard.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_11/SmartHomeDashboard.kt
@@ -7,4 +7,12 @@ fun main() {
         isOnline = true
         powerLoad = 12
     }.also { homeDevices.add(it) }
+
+    SmartDevice("Ezviz Outdoor", "Camera").apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
+        homeDevices.add(it)
+    }
 }
