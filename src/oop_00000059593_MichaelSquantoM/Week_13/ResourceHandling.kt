35f12f46df941986e41eb91a8033dfddf0082fad
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 16:04:15 +0700

    week13: implement manual close for file stream

---
+++ src/oop_00000059593_MichaelSquantoM/Week_13/ResourceHandling.kt
@@ -0,0 +1,12 @@
+package oop_00000059593_MichaelSquantoM.Week_13
+
+import java.io.File
+
+fun main() {
+    println("\n=== TEST SAFE RESOURCE HANDLING ===")
+    val safeFile = File("safe_logs.txt")
+
+    safeFile.printWriter().use { out ->
+        for (i in 1..100) {
+            out.println("Safe Log entry #$i: System status OK.")
+        }
