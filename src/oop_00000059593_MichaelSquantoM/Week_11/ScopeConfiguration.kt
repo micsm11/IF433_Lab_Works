e845bf313bbfb3f6b472f48d3a6e617d39ed0349
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 14:28:45 +0700

    week11: create user class for scope function tests

---
+++ src/oop_00000059593_MichaelSquantoM/Week_11/ScopeConfiguration.kt
@@ -0,0 +1,3 @@
+package oop_00000059593_MichaelSquantoM.Week_11
+
+data class User(var name: String = "", var age: Int = 0)
--- src/oop_00000059593_MichaelSquantoM/Week_11/ScopeLetRun.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_11/ScopeLetRun.kt
@@ -4,17 +4,16 @@ fun main() {
     println("=== TEST LET FUNCTION ===")
     val name: String? = "Alexander"
 
-    val length = name?.let {
+    val nameLength = name?.let {
         println("Nama terdeteksi: $it")
         it.length
     }
-
-    println("Panjang nama: $length")
+    println("Panjang nama: $nameLength")
 
     println("\n=== TEST RUN FUNCTION ===")
     val result = "Kotlin".run {
         println("Memproses kata: ${this}")
-        length * 2
+        this.length * 2
     }
     println("Hasil kalkulasi run: $result")
 }
