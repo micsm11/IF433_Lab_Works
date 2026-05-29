84b1873d5f43746a13bbf295e1ca93a490fb70a7
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 16:15:05 +0700

    week13: (task) implement fromCsvTrade with split

--- src/oop_00000059593_MichaelSquantoM/Week_13/CryptoTradeLogger.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_13/CryptoTradeLogger.kt
@@ -13,3 +13,19 @@ data class TradeRecord(
 fun TradeRecord.toCsv(): String {
     return "$id,$symbol,$type,$margin,$pnl"
 }
+fun fromCsvTrade(line: String): TradeRecord? {
+    return try {
+        val parts = line.split(",")
+
+        TradeRecord(
+            parts[0].toInt(),
+            parts[1],
+            parts[2],
+            parts[3].toDouble(),
+            parts[4].toDouble()
+        )
+    } catch (e: Exception) {
+        println("(Log) Data korup diabaikan: $line")
+        null
+    }
+}
