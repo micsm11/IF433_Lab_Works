ea431ce36000d42d5a960db70714a19f18325663
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 16:20:01 +0700

    week13: (task) implement loadTrades with FileNotFoundException Handling

--- src/oop_00000059593_MichaelSquantoM/Week_13/CryptoTradeLogger.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_13/CryptoTradeLogger.kt
@@ -38,3 +38,10 @@ fun saveTrades(trades: List<TradeRecord>, path: String) {
 }
 fun loadTrades(path: String): List<TradeRecord> {
     return try {
+        File(path)
+            .readLines()
+            .mapNotNull { fromCsvTrade(it) }
+    } catch (e: FileNotFoundException) {
+        emptyList()
+    }
+}
