e6791863debb426a00a548f7abad6561241c1826
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 15:37:30 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000059593_MichaelSquantoM/Week_12/SmartFeeder.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_12/SmartFeeder.kt
@@ -1,4 +1,9 @@
 package oop_00000059593_MichaelSquantoM.Week_12
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
-    }
+    require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    if (isJammed) throw DispenserJamException()
+    if (requestedGram > availableGram) throw FoodEmptyException(requestedGram, availableGram)
+
+}
