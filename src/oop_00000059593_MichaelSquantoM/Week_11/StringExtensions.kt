65e45a9d3450d9bf2a915137f448dfb6bb7b2e33
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 14:23:57 +0700

    week11: create nullable receiver extension

--- src/oop_00000059593_MichaelSquantoM/Week_11/StringExtensions.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_11/StringExtensions.kt
@@ -3,3 +3,11 @@ package oop_00000059593_MichaelSquantoM.Week_11
 fun String.addGreeting(): String {
     return "Hello, $this"
 }
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
+
+fun String?.isNullOrEmptyCustom(): Boolean {
+    return this == null || this.isEmpty()
+}
