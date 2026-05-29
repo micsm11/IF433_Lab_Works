c05375e9987565abf5805b6ace617629170c29b8
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 17:21:48 +0700

    week14: inject Database abstraction to fix DIP

--- src/oop_00000059593_MichaelSquantoM/Week_14/DipRefactored.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_14/DipRefactored.kt
@@ -7,3 +7,6 @@ interface Database {
 class PostgresDatabase : Database {
     override fun query(sql: String): List<String> = listOf("pg_data1", "pg_data2")
 }
+class SafeUserService(private val db: Database) {
+    fun getUser(id: Int) = db.query("SELECT * FROM users WHERE id=$id")
+}
