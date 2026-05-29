fc725ab805cdfda9c8a5f201b95c1b09e30fcd65
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 15:32:07 +0700

    week12: implement basic runCatching

---
+++ src/oop_00000059593_MichaelSquantoM/Week_12/RunCatchingExample.kt
@@ -0,0 +1,8 @@
+package oop_00000059593_MichaelSquantoM.Week_12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+}
