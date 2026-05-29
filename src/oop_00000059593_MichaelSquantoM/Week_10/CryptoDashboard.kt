418597eb650514f46831789ff2c2bd01f2a83932
Michael Squanto <MichaelSquanto@gmail.com>
2026-05-29 14:07:33 +0700

    week10: (task) populate coin repository

--- src/oop_00000059593_MichaelSquantoM/Week_10/CryptoDashboard.kt
+++ src/oop_00000059593_MichaelSquantoM/Week_10/CryptoDashboard.kt
@@ -2,3 +2,6 @@ package oop_00000059593_MichaelSquantoM.Week_10
 
 data class Coin(override val name: String, val balance: Double) : NamedEntity
 data class Transaction(override val name: String, val amount: Double) : NamedEntity
+
+fun main() {
+    val coinRepo = WalletRepository<Coin>()
